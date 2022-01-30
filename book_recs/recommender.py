59017b45ddb243d68e4f412f9d00b973a345033b
Charles Reinertson <charlie.reinertson11@gmail.com>
2022-01-30 11:41:15 -0800

    base recommender system python file

---
+++ book_recs/recommender.py
@@ -0,0 +1,13 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import sys
+import pandas as pd
+from utils import config
+
+
+
+class KNN():
+    def __init__(self):
+        pass
