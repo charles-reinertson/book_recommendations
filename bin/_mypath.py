14c292a435bdd085095308d47566ae59c36daaa2
Urmika Kasi <urmikakasi1999@gmail.com>
2022-02-17 16:41:35 -0800

    sys path bug fix

--- bin/_mypath.py
+++ bin/_mypath.py
@@ -5,8 +5,11 @@ import sys
 thisdir = os.path.dirname(__file__)
 libdir = os.path.join(thisdir, '../')
 
-if libdir not in sys.path:
-    sys.path.insert(0, libdir)
-    sys.path.insert(0, libdir + 'book_recs/')
-    sys.path.insert(0, libdir + 'book_recs/test/')
+
+# if libdir not in sys.path:
+#     sys.path.insert(0, libdir)
+#     sys.path.insert(0, libdir + 'book_recs/')
+#     sys.path.insert(0, libdir + 'book_recs/test/')
+
+os.chdir(libdir)
 
