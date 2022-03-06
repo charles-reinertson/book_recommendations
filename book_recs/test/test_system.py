b6ecb07b89d7e643e48c19901184f86cb378ed06
Charles Reinertson <charlie.reinertson11@gmail.com>
2022-03-06 12:48:27 -0800

    changed tests

--- book_recs/test/test_system.py
+++ book_recs/test/test_system.py
@@ -29,8 +29,8 @@ def test_add_data_sim_book(system_similiar_book):
 def test_get_recommendations_sim_book(system_similiar_book):
     # here are examples ['0971880107', '0316666343', '0385504209', '0060928336', '0312195516']
     recs = system_similiar_book.get_recommendations("0971880107", 5)
-    assert isinstance(recs, list)
-    assert len(recs) == 5
+    assert isinstance(recs, pd.DataFrame)
+    assert recs.shape[0] == 5
 
 def test_add_data_sim_user(system_similar_user):
     before = system_similar_user.data_object.df.shape[0]
@@ -41,5 +41,5 @@ def test_add_data_sim_user(system_similar_user):
 def test_get_recommendations_sim_user(system_similar_user):
     
     recs = system_similar_user.get_recommendations(12432, 10)
-    assert isinstance(recs, list)
+    assert isinstance(recs, pd.DataFrame)
     assert len(recs) == 10
