6bfd64044becc063ddd71aa6a30c59f1bf8e263f
Charles Reinertson <charlie.reinertson11@gmail.com>
2022-03-06 12:41:53 -0800

    deleted matplotlib

--- bin/main.py
+++ bin/main.py
@@ -1,5 +1,3 @@
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 import os
 import sys
@@ -23,11 +21,11 @@ if __name__ == '__main__':
 
     # OPTION 2 - RECOMMENDER SYSTEM ON SIMILAR BOOKS
     
-    # # Instantiate a recommender system with specified features, predicted variable, and recommender type.
-    # recommender_system = RSystem(clean_data=True, recommender_type='similar_book')
-    # # add data at the specified data location. Must be formatted like original input data
-    # recommender_system.add_data(clean_data=True)
-    # # get recommendations for a specific user (here at examples ['0971880107', '0316666343', '0385504209', '0060928336', '0312195516'])
-    # recommendations = recommender_system.get_recommendations('0971880107', num_recommendations=10)
+    # Instantiate a recommender system with specified features, predicted variable, and recommender type.
+    recommender_system = RSystem(clean_data=True, recommender_type='similar_book')
+    # add data at the specified data location. Must be formatted like original input data
+    recommender_system.add_data(clean_data=True)
+    # get recommendations for a specific user (here at examples ['0971880107', '0316666343', '0385504209', '0060928336', '0312195516'])
+    recommendations = recommender_system.get_recommendations('0971880107', num_recommendations=10)
 
-    # print(recommendations)
+    print(recommendations)
