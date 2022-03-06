7e8e5af185882435aa15a8698cd555aa51343397
Charles Reinertson <charlie.reinertson11@gmail.com>
2022-03-06 08:43:56 -0800

    more tests added

--- book_recs/test/test_system.py
+++ book_recs/test/test_system.py
@@ -19,17 +19,17 @@ def system_similar_user():
     return RSystem(clean_data=True, recommender_type='similar_user')
 
 
-# def test_add_data_sim_book(system_similiar_book):
-#     before = system_similiar_book.data_object.df.shape[0]
-#     system_similiar_book.add_data()
-#     after  = system_similiar_book.data_object.df.shape[0]
-#     assert before < after
+def test_add_data_sim_book(system_similiar_book):
+    before = system_similiar_book.data_object.df.shape[0]
+    system_similiar_book.add_data()
+    after  = system_similiar_book.data_object.df.shape[0]
+    assert before < after
 
-# def test_get_recommendations_sim_book(system_similiar_book):
+def test_get_recommendations_sim_book(system_similiar_book):
     
-#     recs = system_similiar_book.get_recommendations("12432", 10)
-#     assert isinstance(recs, list)
-#     assert len(recs) == 10
+    recs = system_similiar_book.get_recommendations("12432", 10)
+    assert isinstance(recs, list)
+    assert len(recs) == 10
 
 def test_add_data_sim_user(system_similar_user):
     before = system_similar_user.data_object.df.shape[0]
