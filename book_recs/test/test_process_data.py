8f8ebc9d57754008d48645628eb990bfc8a5dd2a
Charles Reinertson <charlie.reinertson11@gmail.com>
2022-03-06 08:43:05 -0800

    more tests for system and dataset

--- book_recs/test/test_process_data.py
+++ book_recs/test/test_process_data.py
@@ -40,9 +40,26 @@ def test_bookdataset_unclean(book_data_unclean):
     count_errors = book_data_unclean.df.loc[(book_data_unclean.df["Year-Of-Publication"] > 2022)].shape[0]
     assert count_errors > 0
 
-# def test_bookdataset_new_data_append(book_data_new_append):
-#     pass
+def test_bookdataset_new_data_append(book_data_new_append):
+    size_og = book_data_new_append.df.shape[0]
+    book_data_new_append.append_data(clean_data=True)
+    assert size_og < book_data_new_append.df.shape[0]
 
-    
-data_object = BookDataset()
-print(data_object)
+def test_simple_split_data(book_data_clean):
+
+    x, y = book_data_clean.simple_split_data()
+    assert x.shape[0] > y.shape[0]   
+
+def test_KFold_split_data(book_data_clean):
+
+    train, test = book_data_clean.KFold_split_data()
+    assert train.shape[0] > test.shape[0]   
+
+def test_get_dataframe(book_data_clean):
+    assert isinstance(book_data_clean.get_dataframe(), pd.DataFrame)
+
+def test_get_x_y(book_data_clean):
+    x, y = book_data_clean.get_x_y(["Year-Of-Publication", "ISBN"], ["User-ID"])
+    assert x.columns[0] == "Year-Of-Publication"
+    assert x.columns[1] =="ISBN"
+    assert y.columns[0] == "User-ID"
--- book_recs/test/test_system.py
+++ book_recs/test/test_system.py
@@ -1,6 +1,44 @@
 import _mypath
-from process_data import BookDataset
+from system import RSystem
 import pytest
 
-data_object = BookDataset()
-print(data_object)
+@pytest.fixture
+def system_similiar_book():
+    '''
+    Returns a RSystem instance with similar_book to 
+    get book recommendations based on a provided similar book. 
+    '''
+    return RSystem(clean_data=True, recommender_type='similar_book')
+
+@pytest.fixture
+def system_similar_user():
+    '''
+    Returns a RSystem instance with similar_user to 
+    get book recommendations based on similar users. 
+    '''
+    return RSystem(clean_data=True, recommender_type='similar_user')
+
+
+# def test_add_data_sim_book(system_similiar_book):
+#     before = system_similiar_book.data_object.df.shape[0]
+#     system_similiar_book.add_data()
+#     after  = system_similiar_book.data_object.df.shape[0]
+#     assert before < after
+
+# def test_get_recommendations_sim_book(system_similiar_book):
+    
+#     recs = system_similiar_book.get_recommendations("12432", 10)
+#     assert isinstance(recs, list)
+#     assert len(recs) == 10
+
+def test_add_data_sim_user(system_similar_user):
+    before = system_similar_user.data_object.df.shape[0]
+    system_similar_user.add_data()
+    after  = system_similar_user.data_object.df.shape[0]
+    assert before < after
+
+def test_get_recommendations_sim_user(system_similar_user):
+    
+    recs = system_similar_user.get_recommendations(12432, 10)
+    assert isinstance(recs, list)
+    assert len(recs) == 10
