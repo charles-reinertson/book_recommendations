3a493cd7a75762ab3911fb48569f7283b3c1a391
Charles Reinertson <charlie.reinertson11@gmail.com>
2022-03-06 19:31:15 -0800

    continous integration

--- bin/main.py
+++ bin/main.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     # add data at the specified data location. Must be formatted like original input data
     recommender_system.add_data(clean_data=True)
     # get recommendations for a specific user (also 11676)
-    recommendations = recommender_system.get_recommendations(16795, num_recommendations=10)
+    recommendations = recommender_system.get_recommendations(16795, num_recommendations=5)
 
     print(recommendations)
 
@@ -26,6 +26,6 @@ if __name__ == '__main__':
     # Add data that Must be formatted like original input data
     recommender_system.add_data(clean_data=True)
     # get recommendations for a specific user (here at examples ['0971880107', '0316666343', '0385504209', '0060928336', '0312195516'])
-    recommendations = recommender_system.get_recommendations('0971880107', num_recommendations=10)
+    recommendations = recommender_system.get_recommendations('0971880107', num_recommendations=5)
 
     print(recommendations)
