881ccd0103f787a77ada3012a057081d423d9038
Charles Reinertson <charlie.reinertson11@gmail.com>
2022-03-06 19:44:20 -0800

    test

--- book_recs/utils.py
+++ book_recs/utils.py
@@ -4,6 +4,7 @@ def config(attr):
     Retrieves the queried attribute value from the config file. Loads the
     config file on first call. Use a period when calling items within items
     """
+
     if not hasattr(config, 'config'):
         with open('config.json') as f:
             config.config = eval(f.read())
